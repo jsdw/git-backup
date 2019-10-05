d244a6519bf51e6b2deb5679444e7bee42bd5c7e
James Wilson <james@jsdw.me>
2019-10-05 19:24:34 +0100

    add prune and dry-run options

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod git;
 use error::Error;
 use rayon::prelude::*;
 use std::path::PathBuf;
+use std::collections::HashSet;
 use structopt::StructOpt;
 use services::{ Github, GitLab, Bitbucket, Service };
 
@@ -25,7 +26,14 @@ struct Opts {
     /// this can be provided via the environment variable GIT_TOKEN
     /// instead, but is required in one of those forms.
     #[structopt(long="token")]
-    token: Option<String>
+    token: Option<String>,
+    /// Remove folders in the destination that don't correspond to
+    /// repositories that we have found to back up.
+    #[structopt(long="prune")]
+    prune: bool,
+    /// Don't actually back anything up; just log what we'll do.
+    #[structopt(long="dry-run")]
+    dry_run: bool
 }
 
 fn main() {
@@ -44,6 +52,8 @@ fn run() -> Result<(),Error> {
 
     // Prepare our options:
     let opts = Opts::from_args();
+    let dry_run = opts.dry_run;
+    let prune = opts.prune;
     let url = opts.url;
     let token = opts.token
         .or_else(|| std::env::var("GIT_TOKEN").ok())
@@ -64,25 +74,74 @@ fn run() -> Result<(),Error> {
     }
 
     // Perform the backup:
-    repos.into_par_iter().for_each(|repo| {
+    repos.par_iter().for_each(|repo| {
         log_info!("Syncing '{}'", repo.name);
         let mut repo_path = dest_path.clone();
-        repo_path.push(format!("{}.git", repo.name));
+        repo_path.push(repo_name_to_folder(&repo.name));
 
-        let sync_result = git::sync_repository(git::Opts {
-            repo_url: &repo.git_url,
-            username: &username,
-            password: &token,
-            destination: &repo_path
-        });
-        if let Err(e) = sync_result {
-            log_error!("Could not sync repository '{}': \n{}", repo_path.to_string_lossy(), e);
+        if !dry_run {
+            let sync_result = git::sync_repository(git::Opts {
+                repo_url: &repo.git_url,
+                username: &username,
+                password: &token,
+                destination: &repo_path
+            });
+            if let Err(e) = sync_result {
+                log_error!("Could not sync repository '{}': \n{}", repo_path.to_string_lossy(), e);
+            }
         }
 
     });
+
+    // Prune folders that may have been created with this app
+    // from a prior backup but are now no logner needed.
+    if prune {
+        let keep_these_folders: HashSet<String> = repos
+            .into_iter()
+            .map(|repo| repo_name_to_folder(&repo.name))
+            .collect();
+        for entry in std::fs::read_dir(dest_path)? {
+            // Ignore things we run into an issue reading:
+            let entry = if let Ok(entry) = entry {
+                entry
+            } else {
+                continue
+            };
+            // Ignore non-directories:
+            if !entry.path().is_dir() {
+                continue;
+            }
+            // Ignore non-utf8 filenames (this program wouldn't have created them):
+            let file_name = if let Ok(name) = entry.file_name().into_string() {
+                name
+            } else {
+                continue
+            };
+            // Ignore filenames not ending in '.git':
+            if !file_name.ends_with(".git") {
+                continue
+            }
+            // Ignore filenames for current repos:
+            if keep_these_folders.contains(&file_name) {
+                continue
+            }
+            // Remove the folder and its contents (if not dry_run):
+            log_info!("Pruning {}", file_name);
+            if !dry_run {
+                if let Some(err) = std::fs::remove_dir_all(entry.path()).err() {
+                    log_error!("Error pruning {}: {}", file_name, err);
+                }
+            }
+        }
+    }
+
     log_info!("Backup completed!");
 
     Ok(())
+}
+
+fn repo_name_to_folder(repo_name: &str) -> String {
+    format!("{}.git", repo_name)
 }
 
 fn pick_service(url: String, token: String) -> Option<Box<dyn Service>> {
